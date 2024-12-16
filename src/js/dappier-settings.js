439bb4f02bad0a5d64fc7888f8ff949a1315c4bf
JiveDig <jivetheuniversal@gmail.com>
2024-12-16 16:48:33 -0500

    more js field hide/show logic

--- src/js/dappier-settings.js
+++ src/js/dappier-settings.js
@@ -1,5 +1,28 @@
 document.addEventListener( 'DOMContentLoaded', function() {
-	const colorFields = document.querySelectorAll('.dappier-color-picker');
+	const aiModel           = document.getElementById('aimodel_id');
+	const agentName         = document.querySelector('.agent_name');
+	const agentDesc         = document.querySelector('.agent_desc');
+	const agentPersona      = document.querySelector('.agent_persona');
+	const agentNameInput    = document.getElementById('agent_name');
+	const agentDescInput    = document.getElementById('agent_desc');
+	const agentPersonaInput = document.getElementById('agent_persona');
+	const colorFields       = document.querySelectorAll('.dappier-color-picker');
+	let   fieldsHidden      = false;
+
+	// If no agent value, that means some agents exist but none have been chosen. Hide fields.
+	if ( ! aiModel.value ) {
+		toggleFields( 'none' );
+		fieldsHidden = true;
+	}
+
+	// Hide/show the create agent fields.
+	document.getElementById('aimodel_id').addEventListener('change', function() {
+		// If there is a value and the fields are hidden, show them.
+		if ( aiModel.value && fieldsHidden ) {
+			toggleFields( 'block' );
+			fieldsHidden = false;
+		}
+	});
 
 	// If we have color fields.
 	if ( colorFields.length ) {
@@ -7,5 +30,12 @@ document.addEventListener( 'DOMContentLoaded', function() {
 		colorFields.forEach(field => {
 			jQuery(field).wpColorPicker();
 		});
+	}
+
+	// Function to toggle display.
+	function toggleFields( display ) {
+		agentName.style.display    = display;
+		agentDesc.style.display    = display;
+		agentPersona.style.display = display;
 	}
 });
