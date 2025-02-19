e2d601e47ce46197b12b75e0d393f92f4f4f52ac
JiveDig <jivetheuniversal@gmail.com>
2025-02-19 11:48:22 -0500

    hopefully finally build process to get synced

--- build/block/block.asset.php
+++ build/block/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '9c51005721564a57dcaa');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'b5c2ecb22de62e81b055');
