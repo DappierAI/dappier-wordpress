bb9ca217b51d8ecf3ad9fe60d262ab68121722e0
JiveDig <jivetheuniversal@gmail.com>
2025-03-04 16:34:30 -0500

    settings page tweaks

--- classes/class-settings.php
+++ classes/class-settings.php
@@ -481,7 +481,7 @@ class Dappier_Settings {
 
 		echo '<div class="dappier-step__field">';
 			printf( '<label class="dappier-step__label" for="dappier[askai_location]">%s</label>', __( 'AskAI Location', 'dappier' ) );
-			printf( '<p class="dappier-step__desc">%s</p>', __( 'Select where you would like the AskAI to appear on your site. Use [dappier_askai] shortcode to manually display the askai.', 'dappier' ) );
+			printf( '<p class="dappier-step__desc">%s</p>', __( 'Select where you would like the AskAI to appear on your site. Use the Dappier AskAI block or the [dappier_askai] shortcode to manually display the AskAI module anywhere on your site.', 'dappier' ) );
 
 			echo '<select class="dappier-step__input" name="dappier[askai_location]" id="askai_location">';
 				$options = [
@@ -819,12 +819,10 @@ class Dappier_Settings {
 					// Step 3.
 					echo '<div class="dappier-step dappier-step__three">';
 						echo '<div class="dappier-step__inner">';
-							printf( '<h3 class="dappier-heading">%s</h3>', __( 'Create or choose your default AI Agent', 'dappier' ) );
-							echo '<div class="dappier-step__content">';
-								printf( '<p>%s</p>', __( 'To get started, create or link an existing AskAI agent with your content.', 'dappier' ) );
-								printf( '<p>%s</p>', __( 'Follow the steps below. The setup only takes a few minutes.', 'dappier' ) );
-								printf( '<div class="dappier-callout">%s</div>', __( 'We\'ll securely generate a private AI model, stored in a multi-tenant system. This model will power the Ask AI chatbot and AI-powered recommendations on your site.', 'dappier' ) );
-							echo '</div>';
+							printf( '<h3 class="dappier-heading">%s</h3>', __( 'Linked AskAI Agent', 'dappier' ) );
+							// echo '<div class="dappier-step__content">';
+							// 	printf( '<p>%s</p>', __( '', 'dappier' ) );
+							// echo '</div>';
 							echo '<div class="dappier-step__field dappier-step__agent">';
 								do_settings_fields( 'dappier', 'dappier_three');
 								echo '<details class="dappier-step__advanced agent-advanced">';
@@ -842,7 +840,7 @@ class Dappier_Settings {
 					// Step 4.
 					echo '<div class="dappier-step dappier-step__four">';
 						echo '<div class="dappier-step__inner">';
-							printf( '<h3 class="dappier-heading">%s</h3>', __( 'Configure your site', 'dappier' ) );
+							printf( '<h3 class="dappier-heading">%s</h3>', __( 'Configure your AskAI Agent Settings', 'dappier' ) );
 							echo '<div class="dappier-step__content">';
 								printf( '<p>%s</p>', __( 'Follow the steps below. The setup only takes a few minutes.', 'dappier' ) );
 							echo '</div>';
@@ -893,7 +891,7 @@ class Dappier_Settings {
 								echo '<div class="dappier-step__content">';
 									// If agent is selected.
 									if ( $aimodel_id ) {
-										printf( '<p><strong>%s</strong></p>', __( 'Congratulations! You have linked your Dappier account.', 'dappier' ) );
+										printf( '<p><strong>%s</strong></p>', __( 'Congrats! You have linked your Dappier account.', 'dappier' ) );
 										echo '<ul class="dappier-step__details">';
 										foreach ( $details as $key => $value ) {
 											printf( '<li><span>%s:</span> <span>%s</span></li>', $key, $value );
@@ -1105,18 +1103,19 @@ class Dappier_Settings {
 		unset( $details['queries_used_month'] );
 		unset( $details['total_queries_allowed'] );
 		unset( $details['rev_share_in_percent'] );
+		unset( $details['subscription_level'] );
 
 		// Map the details.
 		$map = [
 			'account_id'            => [ 'label' => __( 'Account ID', 'dappier' ), 'sanitize' => 'sanitize_key' ],
 			'widget_id'             => [ 'label' => __( 'AskAI ID', 'dappier' ), 'sanitize' => 'sanitize_key' ],
 			'email'                 => [ 'label' => __( 'Email', 'dappier' ), 'sanitize' => 'sanitize_email' ],
-			'name'                  => [ 'label' => __( 'Name', 'dappier' ), 'sanitize' => 'sanitize_text_field' ], // TODO: Should this be "Type" or something?
-			'subscription_level'    => [ 'label' => __( 'Subscription', 'dappier' ), 'sanitize' => 'sanitize_text_field' ],
+			'name'                  => [ 'label' => __( 'Subscription', 'dappier' ), 'sanitize' => 'sanitize_text_field' ], // TODO: Should this be "Type" or something?
+			// 'subscription_level'    => [ 'label' => __( 'Subscription', 'dappier' ), 'sanitize' => 'sanitize_text_field' ],
+			'created_at'            => [ 'label' => __( 'Created at', 'dappier' ), 'sanitize' => 'sanitize_text_field' ],
 			'ai_agents'             => [ 'label' => __( 'AI Agents', 'dappier' ), 'sanitize' => 'sanitize_text_field' ],
 			'ai_agents_used'        => [ 'label' => __( 'AI Agents', 'dappier' ), 'sanitize' => 'sanitize_text_field' ],
 			'ai_agents_allowed'     => [ 'label' => __( 'AI Agents', 'dappier' ), 'sanitize' => 'sanitize_text_field' ],
-			'created_at'            => [ 'label' => __( 'Created at', 'dappier' ), 'sanitize' => 'sanitize_text_field' ],
 			'queries'               => [ 'label' => __( 'Queries this Month', 'dappier' ), 'sanitize' => 'sanitize_text_field' ],
 			'queries_used_month'    => [ 'label' => __( 'Queries this Month', 'dappier' ), 'sanitize' => 'sanitize_text_field' ],
 			'total_queries_allowed' => [ 'label' => __( 'Total Queries Allowed', 'dappier' ), 'sanitize' => 'sanitize_text_field' ],
