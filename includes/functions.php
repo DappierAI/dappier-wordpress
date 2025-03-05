b6b4768eec1d80b25774b0d5f0f1a551d6cb8273
JiveDig <jivetheuniversal@gmail.com>
2025-03-05 15:30:28 -0500

    cleanup unused functions since moving to webpack

--- includes/functions.php
+++ includes/functions.php
@@ -68,56 +68,6 @@ function dappier_enqueue_loader() {
 }
 
 /**
- * Get a file version based on last modified date.
- *
- * @since 0.1.0
- *
- * @param string $filename The file name. Example: `dapper`.
- * @param string $type     The file type. Example: `css`.
- * @param bool   $debug    Whether to use the debug version.
- *
- * @return string
- */
-function dappier_get_file_version( $filename, $type, $debug = null ) {
-	$version   = DAPPIER_PLUGIN_VERSION;
-	// $debug     = is_null( $debug ) ? defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG : false;
-	// $path      = $debug ? 'src' : 'build';
-	// $suffix    = $debug ? '' : '.min';
-	$debug     = false;
-	$path      = 'build';
-	$suffix    = '';
-	$filepath  = MAI_ASKNEWS_DIR . "{$path}/{$type}/{$filename}{$suffix}.{$type}";
-	$version  .= '.' . date( 'njYHi', filemtime( $filepath ) );
-
-	ray( $version )->once();
-
-	return $version;
-}
-
-/**
- * Get the URL of a file in the plugin.
- * Checks if script debug is enabled.
- *
- * @since 0.4.0
- *
- * @param string $filename The file name. Example: `dapper`.
- * @param string $type     The file type. Example: `css`.
- * @param bool   $debug    Whether to use the debug version.
- *
- * @return string
- */
-function dappier_get_file_url( $filename, $type, $debug = null ) {
-	// $debug     = is_null( $debug ) ? defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG : false;
-	// $path      = $debug ? 'src' : 'build';
-	// $suffix    = $debug ? '' : '.min';
-	$debug     = false;
-	$path      = 'build';
-	$suffix    = '';
-
-	return DAPPIER_PLUGIN_URL . "{$path}/{$type}/{$filename}{$suffix}.{$type}";
-}
-
-/**
  * Get an option from the dappier options.
  *
  * @since 0.1.0
@@ -143,8 +93,7 @@ function dappier_get_option( $key ) {
  * @return bool
  */
 function dappier_update_option( $key, $value ) {
-	$options = get_option( 'dappier', [] );
-
+	$options         = get_option( 'dappier', [] );
 	$options[ $key ] = $value;
 
 	return update_option( 'dappier', $options );
