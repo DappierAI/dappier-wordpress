0d075147303a773263b72cfcce97ed5d4c54506c
JiveDig <jivetheuniversal@gmail.com>
2025-03-05 11:19:14 -0500

    build

--- build/block/block.asset.php
+++ build/block/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '31b58bec7a51683ac369');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '2b0b1aebf2e0351007b5');
